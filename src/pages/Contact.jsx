003964124b98a568be59db91adc457e9ae0e52c9
Jayakrishna20 <jayakrishnans2041@gmail.com>
2023-11-17 22:08:28 +0530

    Fox animation implemented

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+
+import Fox from "../models/Fox";
+import Loader from "../components/Loader";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -13,6 +22,7 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation("hit");
 
     emailjs
       .send(
@@ -29,80 +39,105 @@ const Contact = () => {
       )
       .then(() => {
         setIsLoading(false);
-        //TODO: Show success message
-        //TODO: Hide an alert
+        showAlert({ show: True, text: "Message sent successfully!", type: "success" });
 
-        setForm({ name: "", email: "", message: "" });
+        setTimeout(() => {
+          hideAlert();
+          setCurrentAnimation("idle");
+          setForm({ name: "", email: "", message: "" });
+        }, [3000]);
       })
       .catch((error) => {
         setIsLoading(false);
+        setCurrentAnimation("idle");
         console.log(error);
-        //TODO: Show error message
+        showAlert({ show: true, text: "I didnt receive your message", type: "danger" });
       });
   };
 
-  const handleFocus = () => {};
-  const handleBlur = () => {};
+  const handleFocus = () => {
+    setCurrentAnimation("walk");
+  };
+  const handleBlur = () => {
+    setCurrentAnimation("idle");
+  };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+      <Alert {...alert} />
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Jaya"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="jaya@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Name
+            <textarea
+              name="message"
+              rows={4}
+              className="textarea"
+              placeholder="Let me know how i can help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
       </div>
 
-      <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
-        <label className="text-black-500 font-semibold">
-          Name
-          <input
-            type="text"
-            name="name"
-            className="input"
-            placeholder="Jaya"
-            required
-            value={form.name}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </label>
-        <label className="text-black-500 font-semibold">
-          Email
-          <input
-            type="email"
-            name="email"
-            className="input"
-            placeholder="jaya@gmail.com"
-            required
-            value={form.email}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </label>
-        <label className="text-black-500 font-semibold">
-          Name
-          <textarea
-            name="message"
-            rows={4}
-            className="textarea"
-            placeholder="Let me know how i can help you!"
-            required
-            value={form.message}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </label>
-        <button
-          type="submit"
-          className="btn"
-          disabled={isLoading}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        >
-          {isLoading ? "Sending..." : "Send Message"}
-        </button>
-      </form>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas camera={{ position: [0, 0, 5], pov: 75, near: 0.1, far: 1000 }}>
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
   );
 };
